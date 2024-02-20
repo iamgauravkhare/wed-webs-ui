ca1068d2115c6034cbbd74c3b2055f01dc7dae04
iamgauravkhare <iamgauravkha@gmail.com>
2024-02-20 18:36:05 +0530

    update ui

--- scripts/script.js
+++ scripts/script.js
@@ -5,14 +5,15 @@ const bgAudio = document.querySelector(".bg-audio");
 const musicPlayBtn = document.querySelector(".music-play-btn");
 const musicPauseBtn = document.querySelector(".music-pause-btn");
 const textAnim = document.querySelectorAll(".text-anim");
+const boxAnim = document.querySelectorAll(".box-anim");
 
-// loaderBtn.addEventListener("click", () => {
-//   loader.style.display = "none";
-//   mainCtn.style.display = "unset";
-//   musicPlayBtn.style.display = "none";
-//   bgAudio.play();
-//   activateGSAP();
-// });
+loaderBtn.addEventListener("click", () => {
+  loader.style.display = "none";
+  mainCtn.style.display = "unset";
+  musicPlayBtn.style.display = "none";
+  bgAudio.play();
+  activateGSAP();
+});
 
 musicPlayBtn.addEventListener("click", () => {
   musicPlayBtn.style.display = "none";
@@ -51,6 +52,77 @@ const activateGSAP = () => {
     });
   });
 
+  boxAnim.forEach((elem) => {
+    return gsap.from(elem, {
+      opacity: 0,
+      duration: 0.5,
+      scrollTrigger: {
+        scroller: "body",
+        trigger: elem,
+        start: "top 85%",
+        end: "top 50%",
+        // markers: true,
+        // scrub: true,
+      },
+    });
+  });
+
+  gsap.from(".sec-eight-svg-anim", {
+    opacity: 0,
+    duration: 2,
+    scrollTrigger: {
+      scroller: "body",
+      trigger: ".sec-eight",
+      start: "top 70%",
+      end: "top 40%",
+      // markers: true,
+      // scrub: true,
+    },
+  });
+
+  gsap.from(".social-icn-ctn", {
+    opacity: 0,
+    scale: 0,
+    y: "30%",
+    duration: 1,
+    scrollTrigger: {
+      scroller: "body",
+      trigger: ".sec-eight-svg-anim",
+      start: "top 85%",
+      end: "top 60%",
+      // markers: true,
+      // scrub: true,
+    },
+  });
+
+  gsap.from(".top", {
+    opacity: 0,
+    y: "20%",
+    duration: 0.5,
+    scrollTrigger: {
+      scroller: "body",
+      trigger: ".top",
+      start: "top 85%",
+      end: "top 60%",
+      // markers: true,
+      // scrub: true,
+    },
+  });
+
+  gsap.from(".bottom", {
+    opacity: 0,
+    y: "40%",
+    duration: 0.5,
+    scrollTrigger: {
+      scroller: "body",
+      trigger: ".bottom",
+      start: "top 85%",
+      end: "top 60%",
+      // markers: true,
+      // scrub: true,
+    },
+  });
+
   const gmss = document.querySelectorAll(".gsap-marker-scroller-start");
 
   gmss.forEach((marker) => {
@@ -75,5 +147,3 @@ const activateGSAP = () => {
     marker.style.zIndex = "20000000";
   });
 };
-
-activateGSAP();
